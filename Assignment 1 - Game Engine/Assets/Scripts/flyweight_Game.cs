647b142134a7e29331c60d8f6b82196e9ce75d48
JonWaaler <jonwaaler@outlook.com>
2018-09-29 23:44:35 -0400

    Flyweight

---
+++ Assignment 1 - Game Engine/Assets/Scripts/flyweight_Game.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class flyweight_Game : MonoBehaviour
+{
+    public Material mat;
+    public GameObject player;
+    private Texture2D flyweightTex;
+    private float t_RateOfSpawn = 0;
+
+    public enum InstancingMode { flyweight,regular};
+    public InstancingMode instMode;
+
+    private void Start()
+    {
+        flyweightTex = new Texture2D(1024, 1024);
+    }
+
+    void Update()
+    {
+        t_RateOfSpawn += Time.deltaTime;
+
+        if (t_RateOfSpawn > .02f)
+        {
+            GameObject inst;
+            if(instMode == InstancingMode.regular)
+                inst = SpawnEnemy_Regular();
+            else
+                inst = SpawnEnemy_Flyweight();
+
+
+            inst.transform.position = transform.position;
+            t_RateOfSpawn = 0;
+        }
+    }
+
+
+    GameObject SpawnEnemy_Regular()
+    {
+        GameObject inst = GameObject.CreatePrimitive(PrimitiveType.Cube);
+//        inst.AddComponent<MeshFilter>();
+        inst.AddComponent<MeshRenderer>();
+        inst.GetComponent<MeshRenderer>().material = new Material(mat);
+        inst.GetComponent<MeshRenderer>().material.mainTexture = new Texture2D(1024,1024);
+        inst.AddComponent<EnemyAI>();
+        inst.GetComponent<EnemyAI>().Player = player;
+        inst.AddComponent<BoxCollider>();
+        inst.AddComponent<CollisionDetectiong>();
+        inst.transform.localScale.Set(2, 4, 2);
+
+        return inst;
+    }
+
+    GameObject SpawnEnemy_Flyweight()
+    {
+        GameObject inst = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        //        inst.AddComponent<MeshFilter>();
+        inst.AddComponent<MeshRenderer>();
+        inst.GetComponent<MeshRenderer>().material = mat;
+        inst.GetComponent<MeshRenderer>().material.mainTexture =flyweightTex;
+        inst.AddComponent<EnemyAI>();
+        inst.GetComponent<EnemyAI>().Player = player;
+        inst.AddComponent<BoxCollider>();
+        inst.AddComponent<CollisionDetectiong>();
+        inst.transform.localScale.Set(2, 4, 2);
+
+        return inst;
+    }
+}
+
---
+++ Assignment 1 - Game Engine/Assets/Scripts/flyweight_Spawner.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flyweight_Spawner : MonoBehaviour
+{
+    public GameObject flyweightInstance;
+
+    public Material flyweight_mat;
+    public Material regular_mat;
+
+    private Texture2D flyWeightTex;
+
+    public enum InstancingMode { flyweight, regular};
+    public InstancingMode instancingMode;
+
+    private void Start()
+    {
+        flyWeightTex = new Texture2D(1024, 1024);
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            if(instancingMode == InstancingMode.flyweight)
+            {
+                for (int i = 0; i < 750; i++)
+                {
+                    // Create a new empty gameobject
+                    GameObject instance = new GameObject("flyweight GM");
+                                     
+                    // here we set the material and mainTexture for the material to a already set variable above
+                    // These vars "flyweight_component" are already in memory so why we put the same data multiple times mem
+                    // when we can just access it
+                    instance.AddComponent<MeshRenderer>().material = flyweight_mat;
+                    instance.GetComponent<MeshRenderer>().material.mainTexture = flyWeightTex;
+
+                }
+            }
+            if (instancingMode == InstancingMode.regular)
+            {
+                for (int i = 0; i < 750; i++)
+                {
+                    // Create a new empty gameobject
+                    GameObject instance = new GameObject("non-flyweight GM");
+                                    
+                    // here we use the "new" keyword to create a new instance of that in memory to test 
+                    // how much more memory non-flyweight uses
+                    instance.AddComponent<MeshRenderer>().material = new Material(regular_mat);
+                    instance.GetComponent<MeshRenderer>().material.mainTexture = new Texture2D(1024,1024);
+                }
+            }
+        }
+    }
+}
