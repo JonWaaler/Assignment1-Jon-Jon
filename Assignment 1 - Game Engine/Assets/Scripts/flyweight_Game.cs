0e938671be25459db9a23901dbcdbab240c7e7bf
Jon Oliver <jon.oliver@uoit.net>
2018-10-01 12:26:13 -0400

    Observer + DLL

--- Assignment 1 - Game Engine/Assets/Scripts/flyweight_Game.cs
+++ Assignment 1 - Game Engine/Assets/Scripts/flyweight_Game.cs
@@ -1,10 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEditor;
 using UnityEngine;
 
 public class flyweight_Game : MonoBehaviour
 {
+    // Dll import & set-up
+    const string DLL_NAME = "Assignment1_DLL";
+    
+    [DllImport(DLL_NAME)]
+    private static extern float random();
+    [DllImport(DLL_NAME)]
+    private static extern float randomRange(int x, int y);
+
+
+    // Flyweight set-up
     public Material mat;
     public GameObject player;
     private Texture2D flyweightTex;
@@ -36,11 +47,11 @@ public class flyweight_Game : MonoBehaviour
         }
     }
 
-
+    // Function that spawns an empty GO and sets it up
     GameObject SpawnEnemy_Regular()
     {
         GameObject inst = GameObject.CreatePrimitive(PrimitiveType.Cube);
-//        inst.AddComponent<MeshFilter>();
+        inst.AddComponent<MeshFilter>();
         inst.AddComponent<MeshRenderer>();
         inst.GetComponent<MeshRenderer>().material = new Material(mat);
         inst.GetComponent<MeshRenderer>().material.mainTexture = new Texture2D(1024,1024);
@@ -49,22 +60,24 @@ public class flyweight_Game : MonoBehaviour
         inst.AddComponent<BoxCollider>();
         inst.AddComponent<CollisionDetectiong>();
         inst.transform.localScale.Set(2, 4, 2);
-
+        
         return inst;
     }
 
     GameObject SpawnEnemy_Flyweight()
     {
         GameObject inst = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        //        inst.AddComponent<MeshFilter>();
+        inst.AddComponent<MeshFilter>();
         inst.AddComponent<MeshRenderer>();
         inst.GetComponent<MeshRenderer>().material = mat;
-        inst.GetComponent<MeshRenderer>().material.mainTexture =flyweightTex;
+        inst.GetComponent<MeshRenderer>().material.mainTexture = flyweightTex;
         inst.AddComponent<EnemyAI>();
-        inst.GetComponent<EnemyAI>().Player = player;
         inst.AddComponent<BoxCollider>();
         inst.AddComponent<CollisionDetectiong>();
         inst.transform.localScale.Set(2, 4, 2);
+
+        // DLL usage
+        inst.GetComponent<MeshRenderer>().material.color = new Color(randomRange(0, 255) / 255.0f, randomRange(0, 255) / 255.0f, randomRange(0, 255) / 255.0f, 1);
 
         return inst;
     }
